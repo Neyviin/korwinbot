1410897c493636d0ed3672d8dcf171da96b51d95
Neyviin <63004456+Neyviin@users.noreply.github.com>
2020-09-20 12:40:55 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const chalk = require("chalk")
-const { Client, MessageEmbed, UserFlags, Message, Channel } = require('discord.js');
+const { Client, MessageEmbed, MessageAttachment  } = require('discord.js');
 
 const client = new Client({ partials: ['MESSAGE', 'CHANNEL', 'REACTION'] });
 
 const Constants = require('./node_modules/discord.js/src/util/Constants.js')
 Constants.DefaultOptions.ws.properties.$browser = `Discord iOS`
 
-const { token, prefix } = require("./source12/config/config.js")
+const { token, prefix, prefix2, prefix3 } = require("./source12/config/config.js")
+
 
 const log = console.log
 const moment = require('moment')
@@ -14,15 +15,13 @@ const ms = require("ms")
 const cache = require("cache")
 
 client.on('ready', () => {
-  log(chalk.blue(`Zalogowano jako ${client.user.tag}!`));
-    // client.user.setActivity(status[rstatus], {type: "WATCaadHING"}); 
-    // You can change the "WATCHING" into STREAMING, LISTENING, and PLAYING.
-    // Example: streaming
-    
+  log(chalk.blue(`Zresetowano ${client.user.tag}!`));
+
+
     client.user.setPresence({
       status: 'Discord iOS',
       activity: {
-          name: 'z feministkami',
+          name: `korwinie pomoc`,
           type: 'PLAYING',
       }
   })
@@ -40,6 +39,7 @@ if (author.bot || !guild) {
 // Ignoruj wiadomości bez prefixu
 if (!msg.content.startsWith(prefix)) return
 
+
 const args = msg.content
 .slice(prefix.length)
 .trim()
@@ -47,7 +47,7 @@ const args = msg.content
 
 const cmd = args.shift().toLowerCase()
 
-if (cmd === 'fakty' || cmd === `<@750329969477025792> fakty`) {
+if (cmd === 'fakty') {
   const fakty = [
     "Podejrzewam, że w dziełach Lenina jest wszystko, jak dobrze poszukać.",
     "Policja, o ile wiem ma pałki. Oprócz tego ma również naboje. Również ostre. Przypominam, że nie po to płacimy podatki na ostre naboje, żeby ich nie używać.",
@@ -88,14 +88,36 @@ if (cmd === 'fakty' || cmd === `<@750329969477025792> fakty`) {
 "Można się tylko cieszyć, że inwalidzi też organizują zawody. Ze sportem nie ma to jednak wiele wspólnego – równie dobrze można by organizować zawody w szachy dla debili.",
 "Kościół jednak nie tylko sprzedaje nadzieję: stanowi organizację przekonującą ludzi, że należy żyć przyzwoicie. Za to warto zapłacić. Inna sprawa, czy nie płacimy za dużo… Zbyt tłusty ksiądz to zazwyczaj zły ksiądz!",
 "Ilość homosiów w każdym kraju jest od pół do jednego, czasami w niektórych krajach, do półtora procenta.",
-`Gdy w organizmie pojawiają się komórki raka, to trzeba je wyciąć i zniszczyć – a nie litować się nad nimi, że są „takie młode” i „takie twórcze".`
+`Gdy w organizmie pojawiają się komórki raka, to trzeba je wyciąć i zniszczyć – a nie litować się nad nimi, że są „takie młode” i „takie twórcze".`,
+"Dla wyjaśnienia moim wyborcom: ja też pijam – ale wyłącznie wino, a na deser miody i (czasem) likier; by nie było, że jestem abstynentem – bo wtedy nie miałbym w wyborach żadnych szans.",
+"Ależ ja jestem za wejściem do strefy wolnego handlu! Tylko że Unia Europejska nie jest taką strefą, a strefą szalejącej biurokracji, która steruje każdym hektolitrem wina i każdą toną wołowiny.",
+"Zdecydowanie wolę, jak pedofil poklepie mi córkę po pupie, niż jak córka pójdzie na lekcję wychowania seksualnego. Po tych lekcjach dziewczyny przestają umieć kochać.",
+"Walczymy z podatkami niesprawiedliwymi. I Stary Testament i Nowy Testament i Koran zawierają zakaz rabunku wdów i sierot. A niczym innym jest podatek spadkowy.",
+"Walką o zachowanie tych cech cywilizacji, dzięki którym szliśmy nieustannie do przodu!",
+"W demokracji wygrywają tylko ci politycy, którzy wygłaszają ogólniki bez treści. Co najwyżej pozwalają sobie na odkrywcze stwierdzenia, że lepiej być zdrowym i bogatym, niż chorym i biednym, że należy dbać o „dobro Polski” – klap, klap i huragan braw!",
+"Ależ ja jestem za wejściem do strefy wolnego handlu! Tylko że Unia Europejska nie jest taką strefą, a strefą szalejącej biurokracji, która steruje każdym hektolitrem wina i każdą toną wołowiny.",
+"Ekonomista tłumaczący, że rynek powinien być wolny, podobny jest dealerowi tłumaczącemu, że kokaina zabija.",
+"Euro-socjalizm to bratni ustrój „realnego socjalizmu”. Z tą różnicą, że real-socjalizm ewoluował od komunizmu do kapitalizmu – a euro-socjalizm – odwrotnie.",
+"Kategorycznie protestuję przeciwko szerzeniu ohydnych kłamstw, jakobym do porannej herbaty sypał 17 łyżeczek cukru! Do szklanki kawy sypię góra 15 łyżeczek cukru",
+"Co to jest de­mok­racja: rządy ludzi, którzy nie znają się na niczym i de­cydują o wszystkim",
+"Idiota z dyp­lo­mem to ta­ki sam idiota, jak przed­tem - tyl­ko z pretensjami.",
+"W Polsce politykę finansują gangi np ci od afery alkoholowej, paliwowej, fozz, węglowej czy ostatnio hazardowej.",
+"Ludzie Epoki Telewizyjnej nie widzą świata takim jaki jest, tylko takim jakim im go pokażą na szklanym ekranie.",
+"De­mok­racja to us­trój, w którym rządzi dur­no­ta - prze­cież wiado­mo, że głupich jest więcej niż mądrych.",
+"Państwa nie mają przyjażni tylko interesy.",
+"Istnieją cztery rodzaje białej śmierci: cukier, sól, kokaina i służba zdrowia.",
+"Jedyną książką jaką warto przeczytać jest Ojciec chrzestny - ta jedna opowiada jak świat jest naprawdę rządzony.",
+"Jak gdzieś jedzie czołg, a ludzie ostrzeżeni się nie usuwają - to ich prawo. Wolny człowiek w wolnym kraju ma prawo dać się rozjechać. Na tym polega liberalizm.",
+"Ludzie nie mają świadomości, bo są trzymani w zagrodzie medialnej,w której sa poddawani dezinformacji, indoktrynacji, kt­óra zawęża im pole widzenia, słyszenia i postrzegania rzeczywistości. Do tego nie mają wyobraźni.",
+
 ];
   const randomIndex = Math.floor(Math.random() * fakty.length);
 channel.send(fakty[randomIndex])
 }
 
 
-if (cmd === "pomoc" || cmd === "<@750329969477025792>" || cmd === "help" || cmd === "info") {
+
+if (cmd === "pomoc" ||  cmd === "help" || cmd === "info") {
   let avatar = (client.user.displayAvatarURL({size: 4096}))
   let link1 = "https://discord.com/oauth2/authorize?client_id=750329969477025792&permissions=387136&scope=bot"
   let link2 = "https://discord.gg/R5PXXm3/"
@@ -105,15 +127,18 @@ const embed = new MessageEmbed()
 .setThumbnail(avatar)
 .setColor("BLUE")
 .setDescription(`[Link do dodania bota](${link1})`)
-.addField("Prefix", "`korwinie`", true)
-.addField("Lista komend", "`fakty` - Podam Ci kilka faktów na temat wszystkiego.")
+.addField("Tyle osób zna moje poglądy", client.users.cache.size.toLocaleString(), true)
+.addField("Znajduję się na tylu serwerach", client.guilds.cache.size.toLocaleString(), true)
+.addField("Prefix", "`korwinie `", true)
+.addField("Lista komend", "`korwinie fakty` - Podam Ci kilka faktów na temat wszystkiego. \n `korwinie poglady` - Powiem, jaka osoba ma poglądy polityczne. \n `korwinie autograf` - Wyślę Ci mój autograf w DM. \n `korwinie losowe` - Losowy obrazek mnie, czyli Korwina.")
 .addField("Developer bota", "`Neyvin#0437`", true)
 .addField("Serwer developerski", `[Link do serwera](${link2})`, true)
 channel.send(embed)
 }
 
-if (cmd === "dodaj" || cmd === "add") {
+if (cmd === "dodaj" || cmd === "add" || cmd === "invite") {
   let link1 = "https://discord.com/oauth2/authorize?client_id=750329969477025792&permissions=387136&scope=bot"
+  let link1920 = "https://discord.gg/R5PXXM3/"
  let embed = new MessageEmbed()
  .setColor("BLUE")
   .setDescription(`[Dodaj mnie!](${link1})`)
@@ -125,16 +150,96 @@ if (cmd === "serwery") {
   if (msg.author.id !== '506486820637376512') return channel.send("To jest tylko i wyłącznie komenda developerska.")
   client.guilds.cache.forEach(server => {
     let total = server.memberCount
+   let bots =  server.members.cache.filter(m => m.user.bot).size
     const embed = new MessageEmbed()
  .setTitle(server.name)
  .setDescription(server.owner.toString())
  .addField("ID", server.id)
  .setColor("RANDOM")
- .addField("Users", total)
+ .addField("Users", `${total} użytkowników w tym ${bots} botów`)
 channel.send(embed)
+
 
   })
 }
+
+if (cmd === "poglady" || cmd === "poglądy") {
+  let user = msg.mentions.users.first() || args[0] && await client.users.fetch(args[0]).catch(() => false) || (msg.author) || (user.id)
+let ball = [ 
+`centrystą.`,
+`prawakiem.`,
+`lewicą.`,
+`monarchistą.`,
+`konserwatystą.`,
+`narodowcem.`,
+`faszystowcem`,
+`feministą.`,
+`feministą, hatfu!`,
+`liberalistą.`,
+`konserwatysto-liberalistą.`,
+`komunistą.`,
+`islamistą.`,
+`socjaldemokratystą.`,
+`socjalistą, hatfu!`,
+`socjalistą.`,
+`demokratystą.`,
+`nacjonalistą.`,
+`libertalistą.`,
+`za Zieloną polityką.`,
+`chrześcijano-narodowcem.`,
+`chrześcijano-demokratystą.`,
+`liberalno-demokratom.`,
+];
+const randomIndex = Math.floor(Math.random() * ball.length);
+  channel.send(`Moim zdaniem ${user.username} jest ${(ball[randomIndex])}`)
+}
+
+if (cmd === "dm") {
+  if (msg.author.id !== '506486820637376512') return channel.send("To jest tylko i wyłącznie komenda developerska.")
+  let user = msg.mentions.users.first() || args[0] && await client.users.fetch(args[0]).catch(() => false) || (msg.author) || (user.id)
+user.send(args.join(" "))
+}
+ 
+if (cmd === "napisz") {
+  let channel = msg.mentions.channels.first() || args[0] && await msg.guild.channels.resolve(args[0]);
+  if (msg.author.id !== '506486820637376512') return channel.send("To jest tylko i wyłącznie komenda developerska.")
+.then(msg => msg.delete({timeout: 3000}) )
+if (!channel) msg.channel.send(args.join(" "))
+.then(msg.delete({timeout: 1}))
+    else channel.send(args.slice(1).join(" "))
+.then(msg.delete({timeout: 1}))
+  }
+
+  if (cmd === "autograf") {
+    channel.send("Wysłane, ciesz się swoim autografem! (jeśli wiadomość nie dotarła, włącz możliwość przyjmowania wiadomości prywatnych)")
+    const attachment = new MessageAttachment('autograf.png');
+    msg.author.send(`O to i twój autograf ${msg.author.username}`, attachment)
+  }
+
+  if (cmd === "media" || cmd === "link" || cmd === "strony" || cmd === "links" || cmd === "linki" || cmd === "linki") {
+    let link3 = "https://www.instagram.com/januszkorwinmikke/?hl=pl"
+    let link4 = "https://www.youtube.com/channel/UCEzucgSFyL5Sh9rF26geBZQ"
+    let link5 = "https://www.facebook.com/janusz.korwin.mikke/"
+    let link6 = "https://twitter.com/JkmMikke?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor"
+    let link7 = "https://discord.com/oauth2/authorize?client_id=750329969477025792&permissions=387136&scope=bot"
+    let embed = new MessageEmbed()
+    .setTitle("Moje socialmedia")
+    .setColor("BLUE")
+    .addField("Twitter", `<:Twitter:756225183949390064> [Link do Twittera](${link6})`)
+    .addField("YouTube", `<:Youtube:756225081474154507> [Link do YouTube'a](${link4})`)
+    .addField("Instagram", `<:Instagram:756225096150155356> [Link do Instagrama](${link3})`)
+    .addField("Facebook", `<:Facebook:756225194074571258> [Link do Facebook'a](${link5})`)
+    .addField("Discord", `<:Discord:756878942178246716> [Link do dodania bota](${link6})`)
+    channel.send(embed)
+    }
+  
+    if (cmd === "zdjecia" || cmd === "zdjecie" || cmd === "losowezdjecie" || cmd === "losowe") {
+      number = 29;
+      let imageNumber = Math.floor(Math.random()* 29) +1
+     channel.send ( {files: ["./zdjecia korwina/" + imageNumber + ".png"]} )
+    }
+
+    
 })
 
 client.login(token); 
---
+++ source12/config/config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    token: "NzUwMzI5OTY5NDc3MDI1Nzky.X049PQ.MdK67NknqQquuhIfhxvzLCbRIGE",
+    prefix: "korwinie ",
+    prefix2: "korwin ",
+    prefix3: "Korwinie ",
+ } 
